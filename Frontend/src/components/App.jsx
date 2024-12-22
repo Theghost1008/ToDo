a89171476b3209880e00335dbcab3c9d9976f758
Theghost1008 <virajchronos@gmail.com>
2024-12-22 14:24:31 +0530

    Remove unnecessary comments

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -1,54 +1,5 @@
-// import React, { useEffect, useState } from "react";
-// import Header from "./Header";
-// import Footer from "./Footer";
-// import Note from "./Note";
-// import CreateArea from "./CreateArea";
-
-
-// function App() {
-//   const [notes, setNotes] = useState([]);
-
-//   function addNote(newNote) {
-//     setNotes(prevNotes => {
-//       return [...prevNotes, newNote];
-//     });
-//   }
-
-//   function deleteNote(id) {
-//     setNotes(prevNotes => {
-//       return prevNotes.filter((noteItem, index) => {
-//         return index !== id;
-//       });
-//     });
-//   }
-
-//   return (
-//     <div>
-//       <Header />
-//       <CreateArea onAdd={addNote} />
-//       {notes.map((noteItem, index) => {
-//         return (
-//           <Note
-//             key={index}
-//             id={index}
-//             title={noteItem.title}
-//             content={noteItem.content}
-//             onDelete={deleteNote}
-//           />
-//         );
-//       })}
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// import LoginForm from "../forms/LoginForm";
-// import RegisterForm from "./forms/RegisterForm";
-// import ForgotPasswordForm from "./forms/ForgotPasswordForm";
 import LoginForm from "../pages/LoginPage";
 import RegisterForm from "../pages/RegistrationPage";
 import ResetPass from "../pages/ResetPass";
