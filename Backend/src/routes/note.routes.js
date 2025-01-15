b73608d1e35594286ce3df04c39b390bf85cb51b
Theghost1008 <virajchronos@gmail.com>
2025-01-15 14:08:39 +0530

    3rd try

--- Backend/src/routes/note.routes.js
+++ Backend/src/routes/note.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { createNote,  getAllNotes, deleteNote} from "../controllers/note.controllers.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
+import { sessionMiddleware } from "../middleware/session.middleware.js";
 
 const router = Router()
 
-router.route("/").post(verifyJWT,createNote)
+router.route("/").post(sessionMiddleware,verifyJWT,createNote)
 
-router.route("/").get(verifyJWT,getAllNotes)
+router.route("/").get(sessionMiddleware,verifyJWT,getAllNotes)
 
-router.route(`/:_id`).delete(verifyJWT,deleteNote)
+router.route(`/:_id`).delete(sessionMiddleware,verifyJWT,deleteNote)
 
 export default router
