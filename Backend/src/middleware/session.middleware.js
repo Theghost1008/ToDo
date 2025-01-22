5b90563feef100fe498506a6fa77b29457961625
Theghost1008 <virajchronos@gmail.com>
2025-01-22 16:44:15 +0530

    Trial dont know

--- Backend/src/middleware/session.middleware.js
+++ Backend/src/middleware/session.middleware.js
@@ -4,16 +4,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const store = connectMongo.create({
-    mongoUrl: `${process.env.MONGODB_URL}/ToDo?retryWrites=true&w=majority`,
-    collectionName: 'sessions',
-    ttl: 60 * 60 // 1 hour
-});
-
-store.on('error', (error) => {
-    console.error('Session Store Error:', error);
-});
-
 const sessionMiddleware = session({
     secret: process.env.SESSION_SECRET || 'default_secret',
     resave: false,
@@ -21,13 +11,13 @@ const sessionMiddleware = session({
     store: connectMongo.create({ 
         mongoUrl: `${process.env.MONGODB_URL}/ToDo?retryWrites=true&w=majority`, 
         collectionName: 'sessions',
-        ttl: 60 * 60 // 1 hour
+        ttl: 1000 * 60 * 60 // 1 hour
     }),
     cookie: {
         maxAge: 1000 * 60 * 60, // 1 hour
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     }
 });
 
